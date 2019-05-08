acca157b42ae81970f077632658960770ac4b33b
Mark Fonte <mark@fonte.com>
2019-05-08 15:23:20 -0400

    pt2

---
+++ client/src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+function Index() {
+    return <h2>Home</h2>;
+}
+
+class About extends React.Component {
+    render() {
+        return(
+            <h2>About</h2>
+        );
+        // fetch("http://localhost:9000/about", {credentials: 'same-origin' })
+        // .then(res => res.text())
+        // .then(res => console.log(res))
+        // .catch(error => console.log(error));
+    }
+}
+
+function Contact() {
+    return <h2>Contact</h2>;
+}
+
+class App extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {apiResponse: ""};
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res}))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                    Edit <code>src/App.js</code> and ssadsave to reload.
+                    </p>
+                    <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Learn React
+                    </a>
+                    <p>{this.state.apiResponse}</p>
+                </header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Route path="/" exact component={Index} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+
+                </div>
+            </Router>
+          );
+    }
+}
+
+export default App;
