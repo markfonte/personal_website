e96378f001969e46e2edc014b4f978386e8bfb3c
Mark Fonte <mark@fonte.com>
2019-05-17 03:00:11 -0400

    connected to firebase firestore

--- api/routes/home.js
+++ api/routes/home.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+var firebaseConfig = {
+    apiKey: "AIzaSyCl2ejn6sSzWijEGZ_mbVP_OyCgTICI4iQ",
+    authDomain: "website-20813.firebaseapp.com",
+    databaseURL: "https://website-20813.firebaseio.com",
+    projectId: "website-20813",
+    storageBucket: "website-20813.appspot.com",
+    messagingSenderId: "249062424082",
+    appId: "1:249062424082:web:0bb18b7464343d1d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("home page!");
-    res.send("Mark Fonte: welcome to my website!");
+    db.collection("page").doc("home").get().then((querySnapshot) => {
+            res.send(`${querySnapshot.data().title_paragraph}`);
+    }).catch(error => console.log(error));
+    // res.send("Mark Fonte: welcome to my website!");
 });
 
 module.exports = router;
