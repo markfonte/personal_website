d86392e9a00359986bcbb89b3a6718526d2ccc04
Mark Fonte <mark@fonte.com>
2019-05-29 15:44:40 -0500

    11 - spotify play complete

--- client/src/random/random.jsx
+++ client/src/random/random.jsx
@@ -4,9 +4,18 @@ import {Typography} from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import TicTacToeGame from './tic_tac_toe_game.jsx';
 
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
+import WebIcon from '@material-ui/icons/Web';
+
 class Random extends React.Component {
   render () {
     const reactTutorialLink = `https://reactjs.org/tutorial/tutorial.html#overview`;
+    const gimbleAcappellaLink = `https://www.gimbleacappella.com/`;
 
     return (
       <div>
@@ -22,6 +31,51 @@ class Random extends React.Component {
             </Link>
           </Typography>
         </div>
+        <Card id="gimble-card">
+          <CardHeader
+            title="Gimble A Cappella"
+            subheader="My acappella group!"
+          />
+          <CardMedia
+            id="gimble-card-media"
+            image={require ('../static/images/gimble_group_pic.jpg')}
+            title="Gimble A Cappella - 2019"
+          />
+          <CardContent>
+            <iframe
+              src="https://open.spotify.com/embed/track/0syVmzCQYedVdmjHtxGy9K"
+              width="300"
+              height="80"
+              title="Dancing On My Own - Gimble A Cappella"
+              frameBorder="8"
+              allowtransparency="true"
+              allow="encrypted-media"
+            />
+            <iframe
+              src="https://open.spotify.com/embed/track/69Wf0jtGtFujPkddCZMdCl"
+              width="300"
+              height="80"
+              title="715 - CR∑∑KS - Gimble A Cappella"
+              frameBorder="8"
+              allowtransparency="true"
+              allow="encrypted-media"
+            />
+            <iframe
+              src="https://open.spotify.com/embed/album/6tgeYaO5FiHOzZuFcIEFTR"
+              width="300"
+              height="220"
+              title="XX - Gimble A Cappella"
+              frameBorder="8"
+              allowtransparency="true"
+              allow="encrypted-media"
+            />
+          </CardContent>
+          <CardActions>
+            <IconButton aria-label="Go to website" href={gimbleAcappellaLink}>
+              <WebIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
       </div>
     );
   }
