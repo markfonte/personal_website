337b8e56b2a34e1eae45ba6528f843ac8d88600f
Mark Fonte <mark@fonte.com>
2019-05-30 16:09:23 -0500

    15 - contact page complete

--- client/src/contact/contact.jsx
+++ client/src/contact/contact.jsx
@@ -1,47 +1,38 @@
 import React from 'react';
 import './contact.css';
-import {Typography} from '@material-ui/core';
+import {Typography, Button} from '@material-ui/core';
 
 class Contact extends React.Component {
-  // constructor(props) {
-  //     super(props);
-  //     this.state = {apiResponse: ""};
-  // }
-
-  // fetchInfo() {
-  //     fetch(process.env.REACT_APP_API_URL + "contact", {credentials: 'same-origin' })
-  //     .then(res => res.text())
-  //     .then(res => this.setState({apiResponse: res}))
-  //     .catch(error => console.log(error));
-  // }
-
-  // componentDidMount() {
-  //     this.fetchInfo();
-  // }
-
   render () {
+    const emailLink = `mailto:mark@fonte.com`;
+    const phoneLink = `tel:406-948-1034`;
+
     return (
       <div>
         <Typography variant="h6">
-          Contact page coming soon...
+          Please contact me in the most convenient way for you!
         </Typography>
-        {/* <form
-          action="mailto:mark@fonte.com?subject=yup"
-          method="post"
-          enctype="text/plain"
+        <Typography variant="subheading">
+          I am always open to new opportunities - recommendations, contracting work,
+          collaborations, job opportunities, etc. Feel free to reach out!
+        </Typography>
+        <Button
+          className="contact-buttons"
+          variant="outlined"
+          color="secondary"
+          href={emailLink}
+          style={{margin: 16}}
         >
-          Name:<br />
-          <input type="text" name="name" /><br />
-          E-mail:<br />
-          <input type="text" name="mail" /><br />
-          Subject:<br />
-          <input type="text" name="subject" /><br />
-          Comment:<br />
-          <input type="text" name="comment" size="50" /><br /><br />
-          <input type="submit" value="Send" />
-          <input type="reset" value="Reset" />
-        </form> */}
-
+          Email
+        </Button>
+        <Button
+          className="contact-button"
+          variant="outlined"
+          color="secondary"
+          href={phoneLink}
+        >
+          Phone
+        </Button>
       </div>
     );
   }
