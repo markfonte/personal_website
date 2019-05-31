cb39e96b028d8a6bd49cbd77c321060241a3d8e1
Mark Fonte <mark@fonte.com>
2019-05-31 00:50:40 -0500

    49 - jpmorgan card complete

--- client/src/current_work/current_work.jsx
+++ client/src/current_work/current_work.jsx
@@ -3,12 +3,14 @@ import './current_work.css';
 import {Typography} from '@material-ui/core';
 import SpotlightCard from './spotlight_card.jsx';
 import DoggieDoodlesCard from './doggie_doodles_card.jsx';
+import JPMorganCard from './jpmorgan_card.jsx';
 
 class CurrentWork extends React.Component {
   render () {
     return (
       <div>
         <Typography variant="h6">What is currently on my plate...</Typography>
+        <JPMorganCard />
         <SpotlightCard />
         <DoggieDoodlesCard />
       </div>
---
+++ client/src/current_work/jpmorgan_card.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './jpmorgan_card.css';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
+import WebIcon from '@material-ui/icons/Web';
+import {Typography} from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+
+class JPMorganCard extends React.Component {
+  render () {
+    const jpmorganLink = `https://www.jpmorganchase.com/`;
+
+    return (
+      <div>
+        <Card raised="true" className="card">
+          <CardHeader
+            title="J.P. Morgan Chase"
+            subheader="Software Engineering Intern"
+          />
+          <CardMedia
+            id="jpmorgan-media"
+            image={require ('../static/images/jpmorgan_logo.jpg')}
+            title="J.P. Morgan Chase logo"
+          />
+          <CardContent>
+            <Typography variant="body1">
+              Software internship in full stack web development. More details to come!
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton aria-label="Go to website" href={jpmorganLink}>
+              <WebIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default JPMorganCard;
