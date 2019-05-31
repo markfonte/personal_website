817d5cae0a3f72b0e42b84f7bf81b7cb1ed26d3f
Mark Fonte <mark@fonte.com>
2019-05-31 14:33:41 -0500

    44 - suggest complete and started learning a-z card

--- client/src/contact/contact.jsx
+++ client/src/contact/contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './contact.css';
 import {Typography, Button} from '@material-ui/core';
+import Link from '@material-ui/core/Link';
 
 class Contact extends React.Component {
   render () {
     const emailLink = `mailto:mark@fonte.com`;
     const phoneLink = `tel:406-948-1034`;
     const venmoLink = `https://venmo.com/Mark-Fonte`;
-
+    const suggestEditLink = `mailto:mark@fonte.com?subject="Website edit suggestion"`;
     return (
       <div>
         <Typography style={{margin: 16}} variant="h6">
@@ -17,33 +18,38 @@ class Contact extends React.Component {
           I am always open to new opportunities - recommendations, contracting work,
           collaborations, job opportunities, etc. Feel free to reach out!
         </Typography>
-        <Button
-          className="contact-buttons"
-          variant="outlined"
-          color="secondary"
-          href={emailLink}
-          style={{margin: 8}}
-        >
-          Email
-        </Button>
-        <Button
-          className="contact-button"
-          variant="outlined"
-          color="secondary"
-          href={phoneLink}
-          style={{margin: 8}}
-        >
-          Phone
-        </Button>
-        <Button
-          className="contact-button"
-          variant="outlined"
-          color="secondary"
-          href={venmoLink}
-          style={{margin: 8}}
-        >
-          Venmo
-        </Button>
+        <div>
+          <Button
+            className="contact-buttons"
+            variant="outlined"
+            color="secondary"
+            href={emailLink}
+            style={{margin: 8}}
+          >
+            Email
+          </Button>
+          <Button
+            className="contact-button"
+            variant="outlined"
+            color="secondary"
+            href={phoneLink}
+            style={{margin: 8}}
+          >
+            Phone
+          </Button>
+          <Button
+            className="contact-button"
+            variant="outlined"
+            color="secondary"
+            href={venmoLink}
+            style={{margin: 8}}
+          >
+            Venmo
+          </Button>
+        </div>
+        <Link variant="caption" color="secondary" href={suggestEditLink}>
+          suggest an edit
+        </Link>
       </div>
     );
   }
