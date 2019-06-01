8620c1d7fe85467c4359ded5b43cb965d331ddc7
Mark Fonte <mark@fonte.com>
2019-06-01 18:25:03 -0500

    57 - home page blurb complete

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -6,7 +6,7 @@ import BuiltWithTable from './built_with_table';
 // import Resume from './resume.jsx';
 
 class Home extends React.Component {
-  render() {
+  render () {
     return (
       <div>
         <Typography style={{margin: 16}} variant="h6">
@@ -14,6 +14,12 @@ class Home extends React.Component {
           4rd year CSE student at the University of Michigan ·
           Minor in Entrepreneurship ·
           Focus on Web &amp; Android development
+        </Typography>
+        <Typography style={{margin: 16}} variant="body1">
+          Welcome to my personal website! I am a computer science in engineering student at the
+          University of Michigan and this website is designed to show you a little more about myself.
+          Please browse around and feel free to check out my social media accounts or contact me
+          directly!
         </Typography>
         <div id="instagram-feed-wrapper">
           <div className="elfsight-app-b21d23fa-3bec-42f0-8828-b41cc63cff02" />
