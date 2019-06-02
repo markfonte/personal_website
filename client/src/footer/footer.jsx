5f1bd73f59a83e1aa80b17429f0faa56874aa1e7
Mark Fonte <mark@fonte.com>
2019-06-02 13:22:30 -0500

    69 - moment.js for date complete

--- client/src/footer/footer.jsx
+++ client/src/footer/footer.jsx
@@ -7,8 +7,9 @@ import {Typography} from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import universityOfMichiganLogoSmall
   from '../static/vectors/university_of_michigan_logo_small.svg';
+import Moment from 'react-moment';
 
-function DisplayError(props) {
+function DisplayError (props) {
   return (
     <Typography variant="title" style={{margin: 16}}>
       Oops! Looks like my backend server is down. Please
@@ -21,32 +22,32 @@ function DisplayError(props) {
 }
 
 class Footer extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
     this.state = {apiResponse: false};
   }
 
-  checkForAPIHeartbeat() {
-    fetch(process.env.REACT_APP_API_URL + 'api_heartbeat', {method: 'HEAD'})
-        .then((res) => this.setState({apiResponse: true}))
-        .catch((err) => {
-          this.setState({apiResponse: false});
-          console.log(err);
-        });
+  checkForAPIHeartbeat () {
+    fetch (process.env.REACT_APP_API_URL + 'api_heartbeat', {method: 'HEAD'})
+      .then (res => this.setState ({apiResponse: true}))
+      .catch (err => {
+        this.setState ({apiResponse: false});
+        console.log (err);
+      });
   }
 
-  componentDidMount() {
-    this.checkForAPIHeartbeat();
-
+  componentDidMount () {
+    this.checkForAPIHeartbeat ();
+    //moment ().startOf ('day').fromNow ();
     const timeoutInterval = 45000; // check for API heartbeat every 45 seconds
-    setInterval(
-        function() {
-          this.checkForAPIHeartbeat();
-        }.bind(this),
-        timeoutInterval
+    setInterval (
+      function () {
+        this.checkForAPIHeartbeat ();
+      }.bind (this),
+      timeoutInterval
     );
   }
-  render() {
+  render () {
     const facebookLink = `https://www.facebook.com/mark.fonte.397`;
     const githubLink = `https://github.com/markfonte`;
     const linkedinLink = `https://linkedin.com/in/mark-fonte/`;
@@ -55,6 +56,7 @@ class Footer extends React.Component {
     const websiteGithubLink = `https://github.com/markfonte/personal_website`;
     const suggestEditLink = `mailto:mark@fonte.com?subject=Website edit suggestion`;
     const stackOverflowLink = `https://stackoverflow.com/users/8266770/mark-fonte`;
+    const updatedDate = '06 02 2019';
 
     return (
       <footer id="footer">
@@ -86,7 +88,9 @@ class Footer extends React.Component {
           </div>
           <div>
             <Typography variant="caption" color="textSecondary">
-              Last updated 6/1/2019
+              Last updated
+              {' '}
+              <Moment parse="MM DD YYYY" fromNow>{updatedDate}</Moment>
             </Typography>
           </div>
           <div>
