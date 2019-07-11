ff9cdd2024b241ae74ea7ef3e9f682e23c823066
Mark Fonte <mark@fonte.com>
2019-07-10 22:55:50 -0500

    82 - add more gimble links to gimble card complete

--- client/src/commit_timestamp.js
+++ client/src/commit_timestamp.js
@@ -1,1 +1,1 @@
-const timestamp = '[07/10/19 21 39 42]'; export default timestamp;
+const timestamp = '[07/10/19 22 55 50]'; export default timestamp;
--- client/src/random/gimble_card.jsx
+++ client/src/random/gimble_card.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './gimble_card.css';
 import WebIcon from '@material-ui/icons/Web';
+import spotifyLogo from '../static/images/logos/spotify_logo.png';
+import googlePlayMusicLogo from '../static/images/logos/google_play_music.svg';
+import appleMusicLogo from '../static/images/logos/apple_music_logo.png';
+import amazonMusicLogo from '../static/images/logos/amazon_music_logo.png';
+import soundcloudLogo from '../static/images/logos/soundcloud_logo.png';
+import youtubeLogo from '../static/images/logos/youtube_logo.png';
+import youtubeMusicLogo from '../static/images/logos/youtube_music_logo.png';
+import facebookLogo from '../static/images/logos/facebook_logo.png';
+import instagramLogo from '../static/images/logos/instagram_logo.png';
+
 import {
   Card,
   CardMedia,
@@ -12,7 +22,28 @@ import {
 
 class GimbleCard extends React.Component {
   render() {
-    const gimbleAcappellaLink = `https://www.gimbleacappella.com/`;
+    const websiteLink = `https://www.gimbleacappella.com/`;
+    const spotifyLink = `https://open.spotify.com/artist/0CgETTTImSmkeCBkoUJiMg?autoplay=true&v=A`;
+    const googlePlayMusicLink = `https://play.google.com/music/m/A6i265eewm2mhu5p2fvikdbf5tm?play=1`;
+    const appleMusicLink = `https://itunes.apple.com/us/artist/gimble-a-cappella/1221861150?ign-gact=3&ls=1`;
+    const amazonMusicLink = `https://music.amazon.com/artists/B06XZ8143C?tab=LIBRARY&ref=dm_wcp_artist_link_s`;
+    const soundcloudLink = `https://soundcloud.com/gimble-a-cappella`;
+    const youtubeLink = `https://www.youtube.com/channel/UCH2dQr26tAWVijoYdKHcgdg`;
+    const youtubeMusicLink = `https://www.youtube.com/channel/UCzwG1_hJ5hRCMqzNlTJj2ZQ`;
+    const facebookLink = `https://www.facebook.com/gimbleacappella/`;
+    const instagramLink = `https://www.instagram.com/gimbleacappella/`;
+
+    const links = [
+      {name: 'Gimble A Cappella on Apple Music', image: appleMusicLogo, link: appleMusicLink},
+      {name: 'Gimble A Cappella on Facebook', image: facebookLogo, link: facebookLink},
+      {name: 'Gimble A Cappella on Spotify', image: spotifyLogo, link: spotifyLink},
+      {name: 'Gimble A Cappella on Google Play Music', image: googlePlayMusicLogo, link: googlePlayMusicLink},
+      {name: 'Gimble A Cappella on SoundCloud', image: soundcloudLogo, link: soundcloudLink},
+      {name: 'Gimble A Cappella on YouTube', image: youtubeLogo, link: youtubeLink},
+      {name: 'Gimble A Cappella on YouTube Music', image: youtubeMusicLogo, link: youtubeMusicLink},
+      {name: 'Gimble A Cappella on Instagram', image: instagramLogo, link: instagramLink},
+      {name: 'Gimble A Cappella on Amazon Music', image: amazonMusicLogo, link: amazonMusicLink},
+    ];
 
     return (
       <div>
@@ -62,9 +93,21 @@ class GimbleCard extends React.Component {
             />
           </CardContent>
           <CardActions>
-            <IconButton aria-label="Go to website" href={gimbleAcappellaLink}>
+            <IconButton aria-label="Go to website" href={websiteLink}>
               <WebIcon />
             </IconButton>
+            {links.map((link, i) => (
+              <IconButton key={i} aria-label={link.name} href={link.link}>
+                <img
+                  src={link.image}
+                  style={{width: '24px', height: '24px'}}
+                  title={link.name}
+                  alt={link.name}
+                />
+              </IconButton>
+            ))}
+
+
           </CardActions>
         </Card>
       </div>
