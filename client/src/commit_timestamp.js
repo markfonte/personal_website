8a589d01b4ffcaa8a644a32b7846dd5f1c41cab2
Mark Fonte <mark@markfonte.dev>
2022-03-18 22:45:18 -0400

    193 - update description on Home

--- client/src/commit_timestamp.js
+++ client/src/commit_timestamp.js
@@ -1,1 +1,1 @@
-const timestamp = '[03/18/22 22 10 21]'; export default timestamp;
+const timestamp = '[03/18/22 22 45 22]'; export default timestamp;
--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -2,7 +2,7 @@ import React, {lazy, Suspense} from 'react';
 import PropTypes from 'prop-types';
 const BuiltWithTableCard = lazy(() => import('./built_with_table_card.jsx'));
 const WelcomeCard = lazy(() => import('./welcome_card.jsx'));
-import {Typography, Link, Tooltip, CircularProgress} from '@material-ui/core';
+import {Typography, Link, CircularProgress} from '@material-ui/core';
 import {withStyles} from '@material-ui/styles';
 import MetaTags from 'react-meta-tags';
 const GlossaryCard = lazy(() => import('../shared/glossary_card.jsx'));
@@ -30,15 +30,15 @@ class Home extends React.Component {
   render() {
     const {classes} = this.props;
 
-    const sdeAmazonLink = 'https://amazon.jobs/en/job_categories/software-development';
+    const justlightLink = 'https://www.justlight.com/about';
     const amazonLink = 'https://www.amazon.com/';
-    const compSciEnginUmichLink = 'https://majors.engin.umich.edu/program/computer-science/';
     const umichLink = 'https://umich.edu/';
     const entrepreneurshipLink = 'https://innovateblue.umich.edu/academics/minor-in-entrepreneurship/';
     const webDevLink = 'https://en.wikipedia.org/wiki/Web_development';
     const androidDevLink = 'https://developer.android.com/';
 
     const lightTheme = getCookie('app_theme') === 'light_theme';
+    const justlightColor = lightTheme ? 'justlight-color-dark' : 'justlight-color-light';
     const amazonColor = lightTheme ? 'amazon-color-dark' : 'amazon-color-light';
     const michiganColor = lightTheme ? 'michigan-color-dark' : 'michigan-color-light';
     const webColor = lightTheme ? 'web-color-dark' : 'web-color-light';
@@ -50,57 +50,46 @@ class Home extends React.Component {
           <title>Home</title>
           <meta
             name="description"
-            content="Incoming Software Development Engineer at Amazon · Student at the University of Michigan"/>
+            content="Director of Software at JustLight · Ex-Amazon · University of Michigan"/>
         </MetaTags>
         <Typography className={classes.title} variant="h6">
-          <Tooltip arrow placement="top" title="Go to Amazon Software Jobs page">
-            <Link color="textSecondary" href={sdeAmazonLink}>
-              <i>Software Development Engineer</i>
-            </Link></Tooltip>{' '}
+          Director of Software at{' '}
 
-          at
+          <Link href={justlightLink} className={justlightColor}>
+            <i>JustLight</i>
+          </Link>
 
-          {' '}<Tooltip arrow placement="top" title="Go to Amazon.com">
-            <Link href={amazonLink} className={amazonColor}>
-              <i>Amazon</i>
-            </Link></Tooltip>{' '}
+          {' '}· Ex-
 
-          ·
+          <Link href={amazonLink} className={amazonColor}>
+            <i>Amazon</i>
+          </Link>
 
-          {' '}<Tooltip arrow placement="top" title="Go to U of M computer science page">
-            <Link color="textSecondary" href={compSciEnginUmichLink}>
-              <i>Computer Science in Engineering</i>
-            </Link></Tooltip>{' '}
+          {' '}Software Engineer · <i>Computer Science Engineering</i> degree from the{' '}
 
-          degree from the
+          <Link href={umichLink} className={michiganColor}>
+            <i>University of Michigan</i>
+          </Link>
 
-          {' '}<Tooltip arrow placement="bottom" title="Go to U of M website">
-            <Link href={umichLink} className={michiganColor}>
-              <i>University of Michigan</i>
-            </Link></Tooltip>{' '}
+          {' '}· Minor in{' '}
 
-          · Minor in
+          <Link color="textSecondary" href={entrepreneurshipLink}>
+            <i>Entrepreneurship</i>
+          </Link>
 
-          {' '}<Tooltip arrow placement="bottom" title="Go to U of M Entrepreneurship page">
-            <Link color="textSecondary" href={entrepreneurshipLink}>
-              <i>Entrepreneurship</i>
-            </Link></Tooltip>{' '}
+          {' '}· Focus on{' '}
 
-          · Focus on
+          <Link href={webDevLink} className={webColor}>
+            <i>Web</i>
+          </Link>{' '}
 
-          {' '}<Tooltip arrow placement="bottom" title="Go to web development page">
-            <Link href={webDevLink} className={webColor}>
-              <i>Web</i>
-            </Link></Tooltip>{' '}
+          &amp;{' '}
 
-          &amp;
+          <Link href={androidDevLink} className={androidColor}>
+            <i>Android</i>
+          </Link>
 
-          {' '}<Tooltip arrow placement="bottom" title="Go to Android Developers page">
-            <Link href={androidDevLink} className={androidColor}>
-              <i>Android</i>
-            </Link></Tooltip>{' '}
-
-          development
+          {' '}development
         </Typography>
         <Suspense fallback={renderLoader()}>
           <WelcomeCard />
