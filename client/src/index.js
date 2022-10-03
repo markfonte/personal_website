4576134430d22e64f407446872b385e2f43218d1
Mark Fonte <mark@fonte.com>
2022-10-02 21:37:48 -0400

    189 - MUI migration worked

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {StrictMode} from 'react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
+    <App />,
 );
 
 // If you want your app to work offline and load faster, you can change
